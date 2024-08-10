45da6f4ea92a3fb8847f35ada2d04f471b204cbc
furqan <furqanullah717@gmail.com>
2024-08-10 23:40:34 +0500

    build fix

--- app/src/main/java/com/codewithfk/expensetracker/android/HomeScreen.kt
+++ app/src/main/java/com/codewithfk/expensetracker/android/HomeScreen.kt
@@ -41,44 +41,9 @@ import com.codewithfk.expensetracker.android.ui.theme.Zinc
 import com.codewithfk.expensetracker.android.viewmodel.HomeViewModel
 import com.codewithfk.expensetracker.android.viewmodel.HomeViewModelFactory
 import com.codewithfk.expensetracker.android.widget.ExpenseTextView
-import com.patrykandpatrick.vico.compose.axis.horizontal.rememberBottomAxis
-import com.patrykandpatrick.vico.compose.axis.vertical.rememberStartAxis
-import com.patrykandpatrick.vico.compose.chart.CartesianChartHost
-import com.patrykandpatrick.vico.compose.chart.layer.rememberLineCartesianLayer
-import com.patrykandpatrick.vico.compose.chart.layer.rememberLineSpec
-import com.patrykandpatrick.vico.compose.chart.rememberCartesianChart
-import com.patrykandpatrick.vico.compose.chart.zoom.rememberVicoZoomState
-import com.patrykandpatrick.vico.compose.component.shape.shader.color
-import com.patrykandpatrick.vico.core.axis.AxisPosition
-import com.patrykandpatrick.vico.core.axis.formatter.AxisValueFormatter
-import com.patrykandpatrick.vico.core.component.marker.MarkerComponent
-import com.patrykandpatrick.vico.core.component.shape.shader.DynamicShaders
-import com.patrykandpatrick.vico.core.model.CartesianChartModelProducer
-import com.patrykandpatrick.vico.core.model.lineSeries
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.withContext
-import kotlin.random.Random
 import android.graphics.Typeface
 import android.text.Layout
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.runtime.remember
-import androidx.compose.ui.unit.dp
-import com.patrykandpatrick.vico.compose.component.fixed
-import com.patrykandpatrick.vico.compose.component.rememberLayeredComponent
-import com.patrykandpatrick.vico.compose.component.rememberLineComponent
-import com.patrykandpatrick.vico.compose.component.rememberShapeComponent
-import com.patrykandpatrick.vico.compose.component.rememberTextComponent
-import com.patrykandpatrick.vico.compose.component.shape.dashedShape
-import com.patrykandpatrick.vico.compose.component.shape.markerCorneredShape
-import com.patrykandpatrick.vico.compose.dimensions.dimensionsOf
-import com.patrykandpatrick.vico.core.chart.dimensions.HorizontalDimensions
-import com.patrykandpatrick.vico.core.chart.insets.Insets
-import com.patrykandpatrick.vico.core.component.shape.Shapes
-import com.patrykandpatrick.vico.core.component.shape.cornered.Corner
-import com.patrykandpatrick.vico.core.component.text.TextComponent
-import com.patrykandpatrick.vico.core.context.MeasureContext
-import com.patrykandpatrick.vico.core.extension.copyColor
-import com.patrykandpatrick.vico.core.marker.Marker
 
 
 @Composable
@@ -308,85 +273,3 @@ fun CardRowItem(modifier: Modifier, title: String, amount: String, imaget: Int) 
 fun HomeScreenPreview() {
     HomeScreen(rememberNavController())
 }
-
-
-@Composable
-internal fun rememberMarker(labelPosition: MarkerComponent.LabelPosition = MarkerComponent.LabelPosition.Top): Marker {
-    val labelBackgroundShape = Shapes.markerCorneredShape(Corner.FullyRounded)
-    val labelBackground =
-        rememberShapeComponent(labelBackgroundShape, MaterialTheme.colorScheme.surface)
-            .setShadow(
-                radius = LABEL_BACKGROUND_SHADOW_RADIUS_DP,
-                dy = LABEL_BACKGROUND_SHADOW_DY_DP,
-                applyElevationOverlay = true,
-            )
-    val label =
-        rememberTextComponent(
-            color = MaterialTheme.colorScheme.onSurface,
-            background = labelBackground,
-            padding = dimensionsOf(8.dp, 4.dp),
-            typeface = Typeface.MONOSPACE,
-            textAlignment = Layout.Alignment.ALIGN_CENTER,
-            minWidth = TextComponent.MinWidth.fixed(40.dp),
-        )
-    val indicatorFrontComponent =
-        rememberShapeComponent(Shapes.pillShape, MaterialTheme.colorScheme.surface)
-    val indicatorCenterComponent = rememberShapeComponent(Shapes.pillShape)
-    val indicatorRearComponent = rememberShapeComponent(Shapes.pillShape)
-    val indicator =
-        rememberLayeredComponent(
-            rear = indicatorRearComponent,
-            front =
-            rememberLayeredComponent(
-                rear = indicatorCenterComponent,
-                front = indicatorFrontComponent,
-                padding = dimensionsOf(5.dp),
-            ),
-            padding = dimensionsOf(10.dp),
-        )
-    val guideline =
-        rememberLineComponent(
-            color = MaterialTheme.colorScheme.onSurface.copy(.2f),
-            thickness = 2.dp,
-            shape = Shapes.dashedShape(
-                shape = Shapes.pillShape,
-                dashLength = 8.dp,
-                gapLength = 4.dp
-            ),
-        )
-    return remember(label, labelPosition, indicator, guideline) {
-        object : MarkerComponent(label, labelPosition, indicator, guideline) {
-            init {
-                indicatorSizeDp = 36f
-                onApplyEntryColor = { entryColor ->
-                    indicatorRearComponent.color = entryColor.copyColor(alpha = .15f)
-                    with(indicatorCenterComponent) {
-                        color = entryColor
-                        setShadow(radius = 12f, color = entryColor)
-                    }
-                }
-            }
-
-            override fun getInsets(
-                context: MeasureContext,
-                outInsets: Insets,
-                horizontalDimensions: HorizontalDimensions,
-            ) {
-                with(context) {
-                    outInsets.top =
-                        (
-                                CLIPPING_FREE_SHADOW_RADIUS_MULTIPLIER * LABEL_BACKGROUND_SHADOW_RADIUS_DP -
-                                        LABEL_BACKGROUND_SHADOW_DY_DP
-                                )
-                            .pixels
-                    if (labelPosition == LabelPosition.AroundPoint) return
-                    outInsets.top += label.getHeight(context) + labelBackgroundShape.tickSizeDp.pixels
-                }
-            }
-        }
-    }
-}
-
-private const val LABEL_BACKGROUND_SHADOW_RADIUS_DP = 4f
-private const val LABEL_BACKGROUND_SHADOW_DY_DP = 2f
-private const val CLIPPING_FREE_SHADOW_RADIUS_MULTIPLIER = 1.4f
